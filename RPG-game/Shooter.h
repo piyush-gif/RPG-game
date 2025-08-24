3b398fb98cc9a6f5f76d3bfb05a1ffd551a3ffed
piyush <piyush.khadka2233@gmail.com>
2025-08-24 10:08:06 +0100

    shooter header

---
+++ RPG-game/Shooter.h
@@ -0,0 +1,22 @@
+//#pragma once
+//#include<SFML/Graphics.hpp>
+//
+//class Shooter {
+//private:
+//	sf::ConvexShape shape;
+//
+//public:
+//	Shooter() {
+//		shape.setPointCount(3);
+//		shape.setPoint(0, sf::Vector2f(10.f, 10.f));
+//		shape.setPoint(1, sf::Vector2f(5.f, 20.f));
+//		shape.setPoint(2, sf::Vector2f(15.f, 20.f));
+//		shape.setFillColor(sf::Color::White);
+//	}
+//
+//	void draw(sf::RenderWindow& window) {
+//		window.draw(shape);
+//	}
+//
+//
+//};
