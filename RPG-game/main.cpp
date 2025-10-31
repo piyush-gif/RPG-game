a81583f3f39f8201d6bd0e6eb90da0a0f38e4bf5
piyush <piyush.khadka2233@gmail.com>
2025-10-31 20:08:36 +0000

    tetris start with sfml

--- RPG-game/main.cpp
+++ RPG-game/main.cpp
@@ -1,18 +1,13 @@
 #include<iostream>
 #include<vector>
 #include<SFML/Graphics.hpp>
-
+using namespace sf;
 int main() {
 
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Tetris");
 	window.setFramerateLimit(60);
 
-	sf::RectangleShape rectangle(sf::Vector2f(700.f, 500.f));
-	rectangle.setPosition(10.f, 10.f);
-	rectangle.setFillColor(sf::Color::Transparent);
-	rectangle.setOutlineThickness(5.f);
-	rectangle.setOutlineColor(sf::Color::White);
-	float speed = 5.f;
+
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -22,8 +17,6 @@ int main() {
 		}
 
 		window.clear(sf::Color::Black);
-
-		window.draw(rectangle);
 
 		window.display();
 	}
